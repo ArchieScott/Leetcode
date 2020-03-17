ac3213671980fb471117094dac8edb3e30931021
ArchieScott <zhousc91@163.com>
2020-03-17 19:27:02 +0800

    add 1~10

--- 0005. Longest Palindromic Substring/0005 Solution & Code - Method 1.cpp
+++ 0005. Longest Palindromic Substring/0005 Solution & Code - Method 1.cpp
@@ -2,13 +2,11 @@
 给定一个字符串 s, 找到 s 中最长的回文子串. 你可以假设 s 的最大长度为 1000.
 
 示例 1:
-
 输入: "babad"
 输出: "bab"
 注意: "aba" 也是一个有效答案.
 
 示例 2:
-
 输入: "cbbd"
 输出: "bb"
 */
@@ -26,6 +24,7 @@ dp[i][j] == true if dp[i + 1][j - 1] == true and str[i] == str[j]
 */
 
 #include <string>
+#include <vector>
 
 class Solution {
 public:
--- 0006. ZigZag Conversion/0006 Solution & Code.cpp
+++ 0006. ZigZag Conversion/0006 Solution & Code.cpp
@@ -1,28 +1,21 @@
 /*
 将一个给定字符串根据给定的行数, 以从上往下从左到右进行 Z 字形排列.
-
 比如输入字符串为 "LEETCODEISHIRING", 行数为 3 时, 排列如下:
-
 L   C   I   R
 E T O E S I I G
 E   D   H   N
-
 之后, 你的输出需要从左往右逐行读取, 产生出一个新的字符串, 比如: "LCIRETOESIIGEDHN".
-
 请你实现这个将字符串进行指定行数变换的函数.
 string convert(string s, int numRows);
 
 示例 1:
-
 输入: s = "LEETCODEISHIRING", numRows = 3
 输出: "LCIRETOESIIGEDHN"
 
 示例 2:
-
 输入: s = "LEETCODEISHIRING", numRows = 4
 输出: "LDREOEIIECIHNTSG"
 解释:
-
 L     D     R
 E   O E   I I
 E C   I H   N
@@ -37,6 +30,8 @@ T     S     G
 E 和 C 相差 2, E 和 O 相差 4, 所以斜字符的一端 step = 2 * row, 另一端 step = interval - step, 
 最后我们修改下标时需要区分 interval 和 step, 当一行字符遍历完成后再 ++row.
 */
+
+#include <string>
 
 class Solution {
 public:
